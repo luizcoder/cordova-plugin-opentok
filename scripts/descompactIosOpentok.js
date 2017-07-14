90602d4796364428f66ffbd13681591eab4fc8db
Danilo Ganzella <dganzella@2mundos.net>
2017-07-14 10:12:12 -0300

    Removed download step of the IOS sdk as it is bugging the install.

---
+++ scripts/descompactIosOpentok.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+module.exports = function (context)
+{
+    var IosSDKVersion = 'OpenTok-iOS-2.8.3';
+    var frameworkDir = context.opts.plugin.dir + '/src/ios';
+    var frameworkDownloaded = frameworkDir + '/OpenTok.framework/download_succesful_' + IosSDKVersion;
+	
+	exec('tar -zxvf ./' + IosSDKVersion + '.tar.bz2', function (err, out, code)
+	{
+		console.log('expanded');
+		exec('rm -r ' + frameworkDir + '/OpenTok.framework', function (err, out, code)
+		{
+			console.log('removed old framework');
+			exec('mv ./' + IosSDKVersion + '/OpenTok.framework ' + frameworkDir, function (err, out, code)
+			{
+				console.log('moved OpenTok.framework into ' + frameworkDir);
+				exec('rm -r ./' + IosSDKVersion, function (err, out, code)
+				{
+					console.log('Removed extracted dir');
+					console.log('Finished');
+				});
+			});
+		});
+	});
+};
